e2af48dec87a5bf8ab038d3e55c4423b729908cd
Jaimin <jaiminpandya@example.com>
2014-05-16 06:37:07 +0530

    Update user.rb and authentication_pages_spec.rb file

--- app/models/user.rb
+++ app/models/user.rb
@@ -22,17 +22,9 @@ class User < ActiveRecord::Base
   validates :password, presence: true, length: { minimum: 6 }
   validates :password_confirmation, presence: true
 
- 
-  def User.new_remember_token
-    SecureRandom.urlsafe_base64
+  private
+  def create_remember_token
+    self.remember_token = SecureRandom.urlsafe_base64
   end
-
-  def User.digest(token)
-    Digest::SHA1.hexdigest(token.to_s)
-  end
-    private
-    def create_remember_token
-      self.remember_token = User.digest(User.new_remember_token)
-    end
 end
 

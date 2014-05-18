fcaff326d76c0c9e53635ffa698352499d34df72
Jaimin <jaiminpandya@example.com>
2014-05-18 14:03:22 +0530

    Modify _header.html.erb file

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -14,12 +14,11 @@ module SessionsHelper
   end
 
   def current_user
-    
     @current_user ||= User.find_by_remember_token(cookies[:remember_token])
   end
 
   def sign_out
-    current_user = nil
+    self.current_user = nil
     cookies.delete(:remember_token)
   end
 end
